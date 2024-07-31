cc3ee9de9b2c307c652ec7f397b15d390f9af4bd
Javier.Barreto <javier.barreto@ENCEDCREMLT0661.local>
2024-07-31 14:20:28 -0600

    Added redux to the project and tested it

--- Frontend/todoapp/src/pages/App.jsx
+++ Frontend/todoapp/src/pages/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { greeting } from '../store/slice';
 
 export const App = () => {
+  const dispatch = useDispatch();
+
   return (
-    <div>App</div>
+    <div>
+      <h2>App</h2>
+
+      <button onClick={() => dispatch(greeting())}>Greeting</button>
+    </div>
   )
 }
