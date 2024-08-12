fd5c8250baca390b2b8b556ba04cd898f3de8bf5
Javier.Barreto <javier.barreto@ENCEDCREMLT0661.local>
2024-08-11 19:29:42 -0600

    avg-per-priority

--- Frontend/todoapp/src/components/Stats.jsx
+++ Frontend/todoapp/src/components/Stats.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
 
 export const Stats = () => {
+  const avgPriorityAll = useSelector(state => state.todos.values.avgPriorityAll)
+  const priorities = useSelector(state => state.todos.values.priorities)
+
   return (
     <div className="row mx-4 border px-4 py-2 mb-4">
       <div className="row">
@@ -16,13 +21,13 @@ export const Stats = () => {
 
         <div className="row">
           <div className="col-6 text-center d-flex align-items-center">
-            <p className="flex-fill">--:-- minutes</p>
+            <p className="flex-fill">{avgPriorityAll}</p>
           </div>
 
           <div className="col-6 text-center">
-            <p className='m-0'>Low: --:-- mins</p>
-            <p className='m-0'>Medium: --:-- mins</p>
-            <p className='m-0'>High: --:-- mins</p>
+            <p className='m-0'>Low: {priorities.avgPriorityLow}</p>
+            <p className='m-0'>Medium: {priorities.avgPriorityMedium}</p>
+            <p className='m-0'>High: {priorities.avgPriorityHigh}</p>
           </div>
         </div>
       </div>
