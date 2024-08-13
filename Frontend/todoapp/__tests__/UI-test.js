8189daf65d88a70201a7b49d310fdd6db4c5d148
Javier.Barreto <javier.barreto@ENCEDCREMLT0661.local>
2024-08-13 17:44:18 -0600

    deleting unecessary files

--- Frontend/todoapp/__tests__/UI-test.js
+++
@@ -1,8 +0,0 @@
-import {fireEvent, render} from '@testing-library/react';
-
-it('OnLOad', () => {
-  const {queryByLabelText, getByLabelText} = render(
-    <CheckboxWithLabel labelOn="On" labelOff="Off" />,
-  );
-
-});
