f8355562569cd8ca0081455488fcbfe4e4240386
JavierABarreto <javier.barreto@ENCEDCREMLT0661.domain.name>
2024-11-27 11:59:47 -0600

    adding buildFilters tests

--- Frontend/src/tests/axios.index.test.js
+++ Frontend/src/tests/axios.index.test.js
@@ -16,28 +16,10 @@ describe('buildFilters Function', () => {
     expect(filters).toEqual(['getBy=Low', 'min=undefined', 'max=undefined']);
   });
 
-  it('returns correct filters for status done', () => {
-    const filters = buildFilters({ getByStatus: 1 });
-    expect(filters).toEqual(['sortByDone=true', 'min=undefined', 'max=undefined']);
-  });
-
-  it('returns correct filters for status undone', () => {
-    const filters = buildFilters({ getByStatus: 2 });
-    expect(filters).toEqual(['sortByUndone=true', 'min=undefined', 'max=undefined']);
-  });
-
-  it('returns correct filters for sorting by priority', () => {
-    const filters = buildFilters({ sortByPriority: 'asc' });
-    expect(filters).toEqual(['sortByPriority=asc', 'min=undefined', 'max=undefined']);
-  });
-
-  it('returns correct filters for sorting by date', () => {
-    const filters = buildFilters({ sortByDate: 'desc' });
-    expect(filters).toEqual(['sortByDate=desc', 'min=undefined', 'max=undefined']);
-  });
-
   it('returns correct filters for multiple parameters', () => {
     const filters = buildFilters({ text: 'test', getByPriority: 'Medium', getByStatus: 1, sortByPriority: 'asc', sortByDate: 'desc' });
     expect(filters).toEqual(['text=test', 'getBy=Medium', 'sortByDone=true', 'sortByPriority=asc', 'sortByDate=desc', 'min=undefined', 'max=undefined']);
   });
+
+
 });
